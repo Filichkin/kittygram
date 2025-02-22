9ce672e317d6910b0133ee7b8f7e7eb37e31bfcf
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-02-22 12:08:20 +0300

    add APIView

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from cats.views import cat_list
+from cats.views import APICat
 
+urlpatterns = [
+    path('cats/', APICat.as_view()),
+]
+
+# from cats.views import cat_list
+
+'''
 urlpatterns = [
    path('cats/', cat_list),
 ]
-
-
+'''
